8988ff17e2419c619777753617c30aa0fa8cb69c
Sabin Chirila <sabin.chirila@gmail.com>
2016-03-16 22:55:03 +0200

    Initial commit of product microservice.

---
+++ product/src/main/java/org/spring/micro/product/App.java
@@ -0,0 +1,13 @@
+package org.spring.micro.product;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
